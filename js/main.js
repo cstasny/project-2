bb1318e518c9ad7e78b51fd29a194d8b220c1625
Clifford <stasser1@outlook.com>
2020-04-28 12:37:23 -0500

    added css styling

--- js/main.js
+++ js/main.js
@@ -40,20 +40,16 @@ $(function () {
     });
 });
 function getData() {
-    //$(function () {
-       //$(".button").click(function () {
-          var input = $(".searchtext").val();
+    var input = $(".search").val();
             var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+input+"+&api_key=8i7DkkKJ0SiHMEDMfoceSri0ATmQmmGw&limit=30");
             xhr.done(function (response) {
                 console.log("success got data", response);
                 var jiffs = response.data;
                 for (i in jiffs) {
-                    $(".results").append("<img src='" + jiffs[i].images.original.url + "' style='height:250px; width:250px;'/>");
-             }
-         })
-     //});
-   //});
- }
+                    $(".results").append("<img src='" + jiffs[i].images.original.url + "' style='width:250px;'/>");
+                }
+            })
+        }
    
 
 
